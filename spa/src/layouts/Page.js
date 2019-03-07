361c2800e626fd65e9faed684a0fa6356c0264d6
danteveill <dante.veill1@wp.pl>
2019-03-07 15:27:07 +0100

    Product Page added

--- spa/src/layouts/Page.js
+++ spa/src/layouts/Page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import ProductListPage from "../pages/ProductListPage";
 import ProductPage from "../pages/ProductPage";
 import ContactPage from "../pages/ContactPage";
 import AdminPage from "../pages/AdminPage";
@@ -12,7 +13,8 @@ const Page = () => {
     <>
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/products" component={ProductPage} />
+        <Route path="/products" component={ProductListPage} />
+        <Route path="/product/:id" component={ProductPage} />
         <Route path="/contact" component={ContactPage} />
         <Route path="/admin" component={AdminPage} />
         <Route path="/login" component={LoginPage} />
